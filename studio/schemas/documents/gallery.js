ebf9dad83df716a138f757a63d725eeb9106bae1
Jai Mata Di <jaimatadi@Jais-iMac.local>
2020-10-29 13:44:34 +0530

    added gallery schema

---
+++ studio/schemas/documents/gallery.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'gallery',
+  type: 'document',
+  title: 'Gallery',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title'
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image'
+    }
+  ]
+}
---
+++ studio/schemas/documents/galleyCategory.js
@@ -0,0 +1,11 @@
+export default {
+  name: 'gallery',
+  type: 'document',
+  title: 'Gallery',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title'
+    }]
+}
