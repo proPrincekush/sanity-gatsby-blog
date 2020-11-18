44663c3b31aa3c42e6401f7ed65afc410d7e58d3
Jai Mata Di <jaimatadi@Jais-iMac.local>
2020-11-18 16:48:29 +0530

    added

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -10,6 +10,7 @@ import category from './documents/category'
 import post from './documents/post'
 import siteSettings from './documents/siteSettings'
 import gallery from "./documents/gallery"
+import homepagePortfolio from "./documents/homepagePortfolio"
 import galleryCategory from "./documents/galleyCategory"
 // Object types
 import bodyPortableText from './objects/bodyPortableText'
@@ -37,7 +38,8 @@ export default createSchema({
     bioPortableText,
     excerptPortableText,
     gallery,
-    galleryCategory
+    galleryCategory,
+    homepagePortfolio
 
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
