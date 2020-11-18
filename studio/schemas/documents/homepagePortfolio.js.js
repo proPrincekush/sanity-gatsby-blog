17ffacb497d9d532ddd8a64c99629784cc7b9006
Jai Mata Di <jaimatadi@Jais-iMac.local>
2020-11-18 16:45:36 +0530

    added new gallery

---
+++ studio/schemas/documents/homepagePortfolio.js.js
@@ -0,0 +1,24 @@
+export default {
+    name: 'gallery',
+    type: 'document',
+    title: 'Gallery',
+    fields: [
+      {
+        name: 'title',
+        type: 'string',
+        title: 'Title'
+      },
+      {
+        name: 'subtitle',
+        type: 'string',
+        title: 'Subtitle'
+      },
+      
+      {
+        name: 'image',
+        type: 'image',
+        title: 'Image'
+      }
+    ]
+  }
+  
